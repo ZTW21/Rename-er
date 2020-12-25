d9252a350790231261383a4b6f45cd2f8f4aff9b
ZTW21 <ZTWilson2002@gmail.com>
2020-12-25 00:16:34 -0500

    added support for non-static holidays

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import threading
 from datetime import *
 import discord
 from dotenv import load_dotenv
+import holidays
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -11,23 +12,26 @@ GUILD = os.getenv("DISCORD_GUILD")
 
 client = discord.Client()
 today = date.today()
+date_str = today.strftime("%d/%m/%Y")
+us_holidays = holidays.US()
 
 
 def change_channel_name():
     if today.month == 1 and today.day == 1:
-        new_name = '🧨 Happy New Years 🎍'
+        new_name = '🧨Happy New Years🎍'
     elif today.month == 2 and today.day == 14:
-        new_name = '💝 Happy Valentines day 💌'
+        new_name = '💝Happy Valentines day💌'
     elif today.month == 7 and today.day == 4:
-        new_name = '🧨 Forth of July 🧨'
+        new_name = '🧨Forth_of_July🧨'
     elif today.month == 12 and today.day == 24:
-        new_name = '🎄 Christmas Eve 🎄'
+        new_name = '🎄Christmas_Eve🎄'
     elif today.month == 12 and today.day == 25:
-        new_name = '🎅 Merry Christmas 🎄'
+        new_name = '🎅Merry_Christmas🎄'
     elif today.month == 12 and today.day == 31:
-        new_name = '🧨 New Years Eve 🧨'
+        new_name = '🧨New_Years_Eve🧨'
     else:
-        new_name = today.strftime("%B %d, %Y")
+        if date(date_str) in us_holidays:
+            new_name = us_holidays.get(date_str)
 
     return new_name
 
